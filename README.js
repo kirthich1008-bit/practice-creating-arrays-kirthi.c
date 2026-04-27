ca04baac4f51bfb3ca4a4d0403b6df38fcae62f9
Kirthi Pasala <kirthipasala@Kirthis-MacBook-Air.local>
2026-04-27 21:25:11 +0530

    arrays

---
+++ README.js
@@ -0,0 +1,11 @@
+let grocery = ["apples", "carrots", "beans", "curd", "milk"];
+let prices = [2, 5, 4, 3, 7];
+
+console.log(grocery);
+console.log(prices);
+console.log(grocery[3]);
+console.log(grocery[4]);
+console.log(grocery[2]);
+console.log(prices[3]);
+console.log(prices[0]);
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# practice-creating-arrays-kirthi.c
